a6c6d027719c314428a931eccdc575db3415dcb7
antazv <82913225+antazv@users.noreply.github.com>
2023-05-30 22:51:53 -0300

    commit

---
+++ LuckyLane/resources/views/menu/menu.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <h1>LuckyLane</h1>
+    </div>
+    <div class="coluna-esquerda">
+        <h4>menu lateral</h4>
+    </div>
+</body>
+</html>
