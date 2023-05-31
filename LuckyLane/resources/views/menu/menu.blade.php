26c6bd9f2d55de4c924cab902fafc5900be751ba
antazv <82913225+antazv@users.noreply.github.com>
2023-05-31 12:01:31 -0300

    comm

--- LuckyLane/resources/views/menu/menu.blade.php
+++ LuckyLane/resources/views/menu/menu.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,26 +8,37 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
+
 <body>
     <div class="header">
         <h1><a href="{{ route('menu') }}" class="float">
-            LuckyLane
-        </a></h1>
-        
+                LuckyLane
+            </a></h1>
+
     </div>
     <div class="main-container">
-        <div class="roleta">
-            Roleta
-            <button><a href="{{ route('roleta') }}">Jogar</a></button>
+        <div class="card">
+            <img src="{{ Vite::asset('resources/images/roleta.png') }}" alt="roleta" style="width:100%">
+            <div class="container">
+                <h3>Roleta</h3>
+                <button><a href="{{ route('roleta') }}">Jogar</a></button>
+            </div>
         </div>
-        <div class="mines">
-            Mines
-            <button><a href="{{ route('mines') }}">Jogar</a></button>
+        <div class="card">
+            <img src="{{ Vite::asset('resources/images/mines.png') }}" alt="mines" style="width:100%">
+            <div class="container">
+                <h3>Mines</h3>
+                <button><a href="{{ route('mines') }}">Jogar</a></button>
+            </div>
         </div>
-        <div class="coin">
-            Coinflip
-            <button><a href="{{ route('coinflip') }}">Jogar</a></button>
+        <div class="card">
+            <img src="{{ Vite::asset('resources/images/coin.png') }}" alt="coin" style="width:100%">
+            <div class="container">
+                <h3>Coinflip</h3>
+                <button><a href="{{ route('coinflip') }}">Jogar</a></button>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
