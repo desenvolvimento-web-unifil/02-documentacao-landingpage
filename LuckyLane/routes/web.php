d1a0a70cc95793cf3ea54db622c9bba64e1106c9
antazv <82913225+antazv@users.noreply.github.com>
2023-06-25 16:15:54 -0300

    commit

--- LuckyLane/routes/web.php
+++ LuckyLane/routes/web.php
@@ -1,44 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-//Rota menu
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-   return view('menu.menu');
-})->name('menu');
+    return view('welcome');
+});
 
-//Rotas jogos
-Route::get('jogos/roleta', function(){return view('jogos.roleta');})->name('roleta');
-Route::get('jogos/mines', function(){return view('jogos.mines');})->name('mines');
-Route::get('jogos/coinflip', function(){return view('jogos.coinflip');})->name('coinflip');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//Rotas CRUD
-Route::get('users', [\App\Http\Controllers\UsersController::class, 'index']);
-Route::get('users/create', [\App\Http\Controllers\UsersController::class, 'create']);
-Route::post('users', [\App\Http\Controllers\UsersController::class, 'store']);
-Route::get('users/edit/{user}', [\App\Http\Controllers\UsersController::class, 'edit']);
-Route::put('users/{user}', [\App\Http\Controllers\UsersController::class, 'update']);
-Route::delete('users/{user}', [\App\Http\Controllers\UsersController::class, 'destroy']);
-
-Route::get('tags', [\App\Http\Controllers\TagController::class, 'index']);
-Route::get('tags/create', [\App\Http\Controllers\TagController::class, 'create']);
-Route::post('tags', [\App\Http\Controllers\TagController::class, 'store']);
-Route::get('tags/edit/{tag}', [\App\Http\Controllers\TagController::class, 'edit']);
-Route::put('tags/{tag}', [\App\Http\Controllers\TagController::class, 'update']);
-Route::delete('tags/{tag}', [\App\Http\Controllers\TagController::class, 'destroy']);
-
-Route::get('categories', [\App\Http\Controllers\CategoryController::class, 'index']);
-Route::get('categories/create', [\App\Http\Controllers\CategoryController::class, 'create']);
-Route::post('categories', [\App\Http\Controllers\CategoryController::class, 'store']);
-Route::get('categories/edit/{category}', [\App\Http\Controllers\CategoryController::class, 'edit']);
-Route::put('categories/{category}', [\App\Http\Controllers\CategoryController::class, 'update']);
-Route::delete('categories/{category}', [\App\Http\Controllers\CategoryController::class, 'destroy']);
-
-
-Route::get('posts', [\App\Http\Controllers\PostController::class, 'index']);
-Route::get('posts/create', [\App\Http\Controllers\PostController::class, 'create']);
-Route::post('posts', [\App\Http\Controllers\PostController::class, 'store']);
-Route::get('posts/edit/{name}', [\App\Http\Controllers\PostController::class, 'edit']);
-Route::put('posts/{name}', [\App\Http\Controllers\PostController::class, 'update']);
-Route::delete('posts/{name}', [\App\Http\Controllers\PostController::class, 'destroy']);
+require __DIR__.'/auth.php';
